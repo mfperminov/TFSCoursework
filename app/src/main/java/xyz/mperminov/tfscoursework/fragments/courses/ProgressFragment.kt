6396743fc0883b010eac3c0e03a0f729d585f96b
Mikhail Perminov <mfperminov@mail.ru>
2019-03-02 13:06:03 +0300

    1. Создать разметку блока «Успеваемость» без учета бейджей и только первые 3 элемента из макета. Вместо изображений использовать разный цвет из ресурсов.

---
+++ app/src/main/java/xyz/mperminov/tfscoursework/fragments/courses/ProgressFragment.kt
@@ -0,0 +1,14 @@
+package xyz.mperminov.tfscoursework.fragments.courses
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import xyz.mperminov.tfscoursework.R
+
+class ProgressFragment: Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_progress, container, false)
+    }
+}
