a7d38eacd83251e55dbc0e7a549daec9b2cfef1d
Mikhail Perminov <mfperminov@mail.ru>
2019-03-04 23:29:20 +0300

    4. Добавить бейджи с баллами в блок «Успеваемость» и SwipeRefreshLayout на экран, на refresh генерировать случайные значения баллов в бейджах. Максимальное число 10, а минимальное 0. Скрывать бейдж если его значение равно нулю.

--- app/src/main/java/xyz/mperminov/tfscoursework/fragments/courses/CoursesFragment.kt
+++ app/src/main/java/xyz/mperminov/tfscoursework/fragments/courses/CoursesFragment.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.get
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_courses.*
+import kotlinx.android.synthetic.main.fragment_progress.*
 import xyz.mperminov.tfscoursework.R
 import xyz.mperminov.tfscoursework.fragments.base.ToolbarTitleSetter
+import xyz.mperminov.tfscoursework.utils.views.BadgeView
 
 
 class CoursesFragment : Fragment() {
@@ -23,7 +27,20 @@ class CoursesFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        (activity as ToolbarTitleSetter).setTitle(getString(R.string.courses))
         super.onViewCreated(view, savedInstanceState)
+        (activity as ToolbarTitleSetter).setTitle(getString(R.string.courses))
+        swipe_layout.setOnRefreshListener {
+            for (i in 0 until profiles_container.childCount)
+                for (j in 0 until (profiles_container[i] as ViewGroup).childCount)
+                    if ((profiles_container[i] as ViewGroup)[j] is BadgeView) {
+                        val currentBadge = (profiles_container[i] as ViewGroup)[j] as BadgeView
+                        val rand = (0..10).random()
+                        currentBadge.text = rand.toString()
+                        if (rand == 0) currentBadge.visibility = View.INVISIBLE else currentBadge.visibility = View.VISIBLE
+                    }
+            swipe_layout.isRefreshing = false
+
+        }
+
     }
 }
