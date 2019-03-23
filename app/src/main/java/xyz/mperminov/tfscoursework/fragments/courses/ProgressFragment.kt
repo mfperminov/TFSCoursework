d2f165e8f22fdbd7587f269f1997dcb8ef6f0508
Mikhail Perminov <mfperminov@mail.ru>
2019-03-23 14:51:34 +0300

    fix potential memory leak with reference to outer class

--- app/src/main/java/xyz/mperminov/tfscoursework/fragments/courses/ProgressFragment.kt
+++ app/src/main/java/xyz/mperminov/tfscoursework/fragments/courses/ProgressFragment.kt
@@ -13,8 +13,9 @@ import kotlinx.android.synthetic.main.fragment_progress.*
 import xyz.mperminov.tfscoursework.R
 import xyz.mperminov.tfscoursework.activities.ContactActivity
 import xyz.mperminov.tfscoursework.utils.views.ProfileView
+import java.lang.ref.WeakReference
 
-class ProgressFragment : Fragment() {
+class ProgressFragment : Fragment(), BadgeUpdateCallback {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_progress, container, false)
     }
@@ -31,33 +32,40 @@ class ProgressFragment : Fragment() {
     }
 
     fun updateBadges() {
-        BadgeUpdateThread(profiles_container.childCount).start()
+        BadgeUpdateThread(this, profiles_container.childCount).start()
     }
 
-    fun setBadge(badgeId: Int, value: Int) {
-        (profiles_container[badgeId] as ProfileView).setBadge(value)
+    override fun setBadge(profileId: Int, badgeValue: Int) {
+        (profiles_container[profileId] as ProfileView).setBadge(badgeValue)
     }
+}
 
-    inner class BadgeUpdateThread(private val profilesCount: Int) : Thread() {
+class BadgeUpdateThread(badgeUpdateCallback: BadgeUpdateCallback, private val profilesCount: Int) : Thread() {
 
-        private val SEND_RANDOM = 10
+    private val reference: WeakReference<BadgeUpdateCallback> = WeakReference(badgeUpdateCallback)
 
-        private val handler = Handler(
-            Looper.getMainLooper(),
-            Handler.Callback { msg ->
-                if (msg.what == SEND_RANDOM) this@ProgressFragment.setBadge(
+    private val SEND_RANDOM = 10
+
+    private val handler = Handler(
+        Looper.getMainLooper(),
+        Handler.Callback { msg ->
+            if (msg.what == SEND_RANDOM)
+                if (reference.get() != null) (reference.get() as BadgeUpdateCallback).setBadge(
                     msg.arg1,
                     msg.arg2
                 ); true
-            })
+        })
 
-        override fun run() {
-            for (i in 0 until profilesCount) {
-                val rand = (0..10).random()
-                val message = handler.obtainMessage(SEND_RANDOM, i, rand)
-                handler.sendMessage(message)
-            }
+    override fun run() {
+        for (i in 0 until profilesCount) {
+            val rand = (0..10).random()
+            val message = handler.obtainMessage(SEND_RANDOM, i, rand)
+            handler.sendMessage(message)
         }
     }
+}
+
+interface BadgeUpdateCallback {
+    fun setBadge(profileId: Int, badgeValue: Int)
 }
 
